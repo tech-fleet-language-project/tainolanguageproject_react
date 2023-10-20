267c88645362b7534ac009afa3d9a44772a4e83f
Okino Kamali Leiba <17099286+OkinoLeiba@users.noreply.github.com>
2023-10-20 17:56:21 -0400

    major chnage

--- constants/add-firebase.tsx
+++ constants/add-firebase.tsx
@@ -4,8 +4,8 @@ async function addFirebase(projectId) {
   const accessToken = getAccessToken();
   const uri = 'https://firebase.googleapis.com/v1beta1/projects/' + projectId + ':addFirebase';
   const options = {
-    method: 'POST',
-    // Use a manual access token here since explicit user access token is required.
+    method: 'GET',
+    // use a manual access token here since explicit user access token is required.
     headers: {
       'Authorization': 'Bearer ' + accessToken,
     },
@@ -14,6 +14,7 @@ async function addFirebase(projectId) {
   try {
     const rawResponse = await fetch(uri, options);
     const resp = await rawResponse.json();
+    // log response and error in database
     console.log(resp);
   } catch(err) {
     console.error(err['message']);
